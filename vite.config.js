b37ef32f354cb17e05d3cece996d47d42d2cafbf
KEERTHI9030 <2500030147@kluniversity.in>
2026-02-23 21:58:53 +0530

    before deploy

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,7 @@
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import react from '@vitejs/plugin-react'
 
-// https://vite.dev/config/
 export default defineConfig({
-   base:'/lab5signup',
   plugins: [react()],
+  base: "/lab5signup/"   // 👈 MUST be your repo name
 })
